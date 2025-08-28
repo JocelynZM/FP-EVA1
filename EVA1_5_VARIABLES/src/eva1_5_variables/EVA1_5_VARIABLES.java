99c7fb2dcfe4e0de3ad3f0601160d37128df5af8
Jocelyn Zamarron Mendoza <jozzamarron@gmail.com>
2025-08-27 22:11:01 -0600

    Add files via upload

---
+++ EVA1_5_VARIABLES/src/eva1_5_variables/EVA1_5_VARIABLES.java
@@ -0,0 +1,23 @@
+
+package eva1_5_variables;
+
+public class EVA1_5_VARIABLES {
+
+    public static void main(String[] args) {
+        //DECLARACION VARIABLES
+        //CREARLAS LAS VARIABLES PARA QUE SE PUEDAN USAR
+        //TIPO DE DATO Y LUEGO EL IDENTIDICADOR
+        String Nombre;
+        byte edad;
+        //INICALIZACION
+        //USAMOS EL OPERADOR = ---> OPERADOR DE ASIGNACION
+        // PON  "JOCELYN ZAMARRON MEDNOZA" DENTRO DE LA VARIABLE NOMBRE
+        Nombre = "JOCELYN ZAMARRON MENDOZA";
+        edad = 18;
+        //USO DE VARIABLES
+        System.out.println(Nombre);
+        System.out.println(edad);
+
+    }
+    
+}
